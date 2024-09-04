675e7193e90557c0c5e578b06a84af7ef0f7b82d
BeriutaH <13249837053@163.com>
2024-09-04 14:10:45 +0800

    服务发现与注册2

--- xclient/discovery_gee.go
+++ xclient/discovery_gee.go
@@ -3,10 +3,10 @@ package xclient
 import "time"
 
 type GeeRegistryDiscovery struct {
-	*MultiServersDiscovery
-	registry   string
-	timeout    time.Duration
-	lastUpdate time.Time
+	*MultiServersDiscovery               // 嵌套了 MultiServersDiscovery，很多能力可以复用
+	registry               string        // 注册中心的地址
+	timeout                time.Duration // 服务列表的过期时间
+	lastUpdate             time.Time     // 代表最后从注册中心更新服务列表的时间，默认 10s 过期之后，需要从注册中心更新新的列表
 }
 
 const defaultUpdateTimeout = time.Second * 10
